4d17ee25e8ca15224187d2640543354bd8163085
nikomu <46274588+mkniko25@users.noreply.github.com>
2019-01-26 17:00:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-// // 基本　ボタンを押すと出て消える
-// $(function () {
-		
-// 	// $("button").click(function(){
-// 	$(".pic2").fadeIn();
-// 	$(".pic2").fadeOut();
-// 	// });
-// });
-
-
-
-// var form = $('form');
-// form.submit(function(e) {
- 
-// 	// //ここで何か処理を行う
-//   	$(".pic2").fadeIn();
-//  	$(".pic2").fadeOut();
-
-//  // 	// alert('aaa');
-
-// 	//3秒遅れで発生したsubmitイベントを消してからsubmit
-// 	setTimeout( function() {
-// 		form.off('submit');
-// 		form.submit();
-
-// 	}, 1000);
- 
-// 	//自動でsubmitされないように処理を止める
-//     return false;
-    
-
-// } );
-
-
 //攻撃の時、稲妻を表示する
 $(function () {
 
@@ -59,43 +25,6 @@ $(function () {
         };
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(function () {
-
-// 	$("button").click(function(){
-// $('body').append('<div class="loading"><img src="img/inazuma.png"></div>');
-// });
-// });
-
-
-// $('input[name="attack"]').on('click',function(){
-// 	setTimeout(function(){
-//         $(".pic2").fadeIn()},100)
-// });
-
-// $('input[name="attack"]').on('click',function(){
-
-// setTimeout(function(){
-// 	// $('.pic2').append('<div class="loading"><img src="img/inazuma.png"></div>');},100);
-
-// 	// setTimeout(function(){
-//  //        $('body').append('<div class="loading"><img src="img/inazuma.png" /></div>');},5000);
-
-// });
-
 
 
 
